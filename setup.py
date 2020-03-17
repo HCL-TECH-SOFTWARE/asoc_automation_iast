79e24b5897874da2013035c038432f0755339e56
trabetti-hcl <61489860+trabetti-hcl@users.noreply.github.com>
2020-03-17 13:27:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='asoc-automation-iast',
+      version='0.1',
+      description='asoc interfact for handling iast',
+      url='http://github.com/hclproducts/asoc-automation-iast',
+      author='Tali Rabetti',
+      author_email='tali.raetti@hcl.com',
+      packages=['asoc-automation-iast'],
+      zip_safe=False)
