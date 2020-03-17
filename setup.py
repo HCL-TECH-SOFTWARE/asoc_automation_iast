0e35a3cb412b3c73d662042642b5cf83e0a9dbd3
Tali Rabetti <tali.rabetti@hcl.com>
2020-03-17 15:07:50 +0200

    remove wrong attempts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='asoc_automation_iast',
       version='0.1',
-      description='asoc interfact for handling iast',
+      description='asoc interface for handling iast',
       url='http://github.com/hclproducts/asoc_automation_iast',
       author='Tali Rabetti',
       author_email='tali.raetti@hcl.com',
