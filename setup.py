beb3a69b25dd5d77bb834c81f542a97b0976f795
Tali Rabetti <tali.rabetti@hcl.com>
2020-06-02 09:02:04 +0300

    fix email address

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='asoc_automation_iast',
       description='asoc interface for handling iast',
       url='http://github.com/hclproducts/asoc_automation_iast',
       author='Tali Rabetti',
-      author_email='tali.raetti@hcl.com',
+      author_email='tali.rabetti@hcl.com',
       packages=['asoc_automation_iast'],
       zip_safe=False, install_requires=['urllib3', 'requests'])
