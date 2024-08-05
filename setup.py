49b3ade58838ac731676c0249bb8b5bab7dd8e5f
Tali <61489860+trabetti-hcl@users.noreply.github.com>
2024-08-05 06:18:11 +0300

    disable email notification by default (#14)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='asoc_automation_iast',
-      version='0.19',
+      version='0.20',
       description='asoc interface for handling iast',
       url='https://github.com/HCL-TECH-SOFTWARE/asoc_automation_iast',
       author='Tali Rabetti',
